46094362d0a144e7f694d296ec992e4228cfcf40
Manav <ifightwars@gmail.com>
2022-05-26 22:16:54 -0700

    routes added

---
+++ app.py
@@ -0,0 +1,23 @@
+import config
+from flask import Flask, render_template
+from web3 import Web3
+
+app = Flask(__name__)
+w3 = Web3(Web3.HTTPProvider(config.INFURA_URL))
+
+@app.route("/")
+def index():
+    return "Etherbrowse"
+
+@app.route("/tx/<hash>")
+def transaction(hash):
+    transaction = w3.eth.get_transaction(hash)
+    return render_template("transaction.html", hash=hash, transaction = transaction)
+
+@app.route("/address/<addr>")
+def address(addr):
+    return render_template("address.html", addr=addr)
+
+@app.route("/block/<block_number>")
+def block(block_number):
+    return render_template("block.html", block_number=block_number)
